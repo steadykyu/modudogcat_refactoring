9ef61dea197734f2dd718c0e71b78681616b0b41
steadykyu <awakekyu1@gmail.com>
2024-01-13 18:48:02 +0900

    increase Jwt Token expiration time

--- src/main/java/com/k5/modudogcat/ModudogcatApplication.java
+++ src/main/java/com/k5/modudogcat/ModudogcatApplication.java
@@ -8,12 +8,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @EnableJpaAuditing
-//@SpringBootApplication(exclude = SecurityAutoConfiguration.class) //TempTest: 임시 추가 - 로그인 확인하려면 제거해야 함
 @SpringBootApplication
 public class ModudogcatApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ModudogcatApplication.class, args);
-	// NOTE: 페이지 네이션 점검하기
+
 	}
 }
