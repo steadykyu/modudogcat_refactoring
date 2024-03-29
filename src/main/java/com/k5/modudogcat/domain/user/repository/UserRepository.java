34a8c991f10718db2e583337d7f6c2946598d841
steadykyu <awakekyu1@gmail.com>
2024-03-29 16:24:17 +0900

    "Role을 지연로딩으로 설정 및 fetch join을 통한 로그인 기능 동작"

--- src/main/java/com/k5/modudogcat/domain/user/repository/UserRepository.java
+++ src/main/java/com/k5/modudogcat/domain/user/repository/UserRepository.java
@@ -4,12 +4,16 @@ import com.k5.modudogcat.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByLoginId(String LoginId);
+//    Optional<User> findByLoginId(String LoginId);
+    @Query("select u from user_table u join fetch u.roles where u.loginId = :loginId")
+    Optional<User> findByLoginId(@Param("loginId") String LoginId);
     Optional<User> findByEmail(String email);
     Page<User> findAllByUserStatus(User.UserStatus userStatus, Pageable pageable);
 }
--- src/main/java/com/k5/modudogcat/security/userdetails/UserDetailsServiceImpl.java
+++ src/main/java/com/k5/modudogcat/security/userdetails/UserDetailsServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
